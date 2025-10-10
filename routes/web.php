2c422709a943c59db6c1fbd1e395492bb07364ca
Otabek <otabekshavkatov956@gmail.com>
2025-10-10 17:48:12 +0500

    fixed autarihation

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,15 @@ use App\Http\Controllers\AuthController;
 
 Route::group([
     'prefix' => '{prefix?}', 
-    'where' => ['prefix' => 'backm|backs|mizan|tmk'] // shu so‘zlardan biri bo‘lishi yoki bo‘sh bo‘lishi mumkin
+    'where' => ['prefix' => 'backm|backs|mizan|tmk'] 
 ], function () {
-    // Login
+    
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
-    // SMS verify
     Route::get('/sms-verify', [AuthController::class, 'showSmsVerify'])->name('sms.verify');
     Route::post('/sms-verify', [AuthController::class, 'verifySms'])->name('sms.verify.submit');
     Route::post('/sms-resend', [AuthController::class, 'resendSms'])->name('sms.resend');
 
-    // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
